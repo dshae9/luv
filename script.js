e99d6666a27cf1aa67f77ac2e9df6ee7d80001cd
Glitch (hello-webpage) <none>
2021-01-29 21:20:10 +0000

    🚖🏐 Checkpoint ./script.js:3635283/452 ./index.html:3635283/125

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-var items = ["You're cool" , ];
 
-var item = items[Math.floor(Math.random() * items.length)];
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var items = ["You're cool" , "You're sweet" , "Be mine" , "All yours" , "Sweetheart"];
+console.log(random_item(items));
