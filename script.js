d40dbcf084f92fd1584f7cffcea80d60fdfd3312
Glitch (hello-webpage) <none>
2021-02-04 06:11:37 +0000

    💨📅 Checkpoint ./script.js:3635283/328

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ let btnRandom = document.querySelector('button');
 let result = document.querySelector('p');
 
 let list = ["You're cool" , "You're sweet" , "Be mine" , "All yours" , "Sweetheart"];
-btnRandom.addEventListener('click', () => {});
 
 
+function getRandomNumber(min, max){
+  let step1 = max - min +1;
+  let step2 = Math.random() * step1;
+  let result = Math.floor(step2) + min;
+  
+  return result;
+}
 
-
+btnRandom.addEventListener('click', () => {
+  
+  let index = getRandomNumber(0, list.len)
+});
 
 
 
