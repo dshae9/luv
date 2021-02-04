473ce35e4d81858be4e9d517575f2cd08d294402
Glitch (hello-webpage) <none>
2021-02-04 06:00:12 +0000

    🎺🚅 Checkpoint ./script.js:3635283/271 ./index.html:3635283/37

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-var items = ["You're cool" , "You're sweet" , "Be mine" , "All yours" , "Sweetheart"];
-function random_item(items)
-{
+let btnRandom = document.querySelector('button');
+let result = document.querySelector('p');
+
+let list = ["You're cool" , "You're sweet" , "Be mine" , "All yours" , "Sweetheart"];
+btnRandom.addEventListener('click', () => {});
+
+
+
+
+
+
+
+
+
+
+
+
+// var items = ["You're cool" , "You're sweet" , "Be mine" , "All yours" , "Sweetheart"];
+// function random_item(items)
+// {
   
-  document.getElementById("items").console.log(random_item(items));
+//   document.getElementById("items").console.log(random_item(items));
   
-return items[Math.floor(Math.random()*items.length)];
+// return items[Math.floor(Math.random()*items.length)];
      
-}
+// }
 
 
-console.log(random_item(items));
+// console.log(random_item(items));
